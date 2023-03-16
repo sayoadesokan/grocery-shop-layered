88bbbe593959d519b2dcf65f4e44e8eefc68eb0c
Adesayo Adesokan <sayosokan@gmail.com>
2023-03-16 13:45:05 +0100

    third commit

--- src/api/shopping-api.js
+++ src/api/shopping-api.js
@@ -9,5 +9,32 @@ module.exports = (app) => {
   app.post('/shopping/order', userAuth, async (req, res, next) => {
     const { _id } = req.user;
     const { txnNumber } = req.body;
+    try {
+      const { data } = await service.PlaceOrder({ _id, txnNumber });
+      return res.status(200).json(data);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  app.get('/shopping/orders', UserAuth, async (req, res, next) => {
+    const { _id } = req.user;
+
+    try {
+      const { data } = await userService.getShoppingDetails(_id);
+      return res.status(200).json(data.orders);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  app.get('/shopping/cart', UserAuth, async (req, res, next) => {
+    const { _id } = req.user;
+    try {
+      const { data } = await userService.getShoppingDetails(_id);
+      return res.status(200).json(data.cart);
+    } catch (err) {
+      next(err);
+    }
   });
 };
